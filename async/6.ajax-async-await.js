06f3781f4b9140d755775ead3c6a68e9e5d44c25
Bishwa Poudel <bispoul181@gmail.com>
2018-05-19 19:02:42 +0545

    Add error handling

--- async/6.ajax-async-await.js
+++ async/6.ajax-async-await.js
@@ -1,12 +1,21 @@
 // doing same as before using async-await
 
-
+// every data returned by async function is a promise. So, use then to handle the data
 const showTempAW = async (woeid) => {
-    const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
-    const data = await result.json()
-    console.log(`The temperature today in ${data.title} will be between ${data.consolidated_weather[0].min_temp} and ${data.consolidated_weather[0].max_temp} degree Celsius.`)
+    try {
+        const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
+        const data = await result.json()
+        console.log(`The temperature today in ${data.title} will be between ${data.consolidated_weather[0].min_temp} and ${data.consolidated_weather[0].max_temp} degree Celsius.`)
+
+        return data
+    }
+    catch(error){
+        console.log('Oops!! An error occured. Try again later !!')
+    }
 }
 
 showTempAW(44418)
-showTempAW(2487956)
-console.log('Hello')
+showTempAW(2487956).then(data => {
+    const dataWeather = data
+    console.log('Data successfully stored.')
+})
